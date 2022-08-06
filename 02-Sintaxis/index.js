5b3d93656d654c5acd4e713549d0f252564d94ee
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2022-08-06 19:21:49 -0400

    Add ejercicio 02-Sintaxis

---
+++ 02-Sintaxis/index.js
@@ -0,0 +1,26 @@
+/**
+ * Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+ */
+
+const miInformacion = [
+  "Jean Carlos Trejo",
+  26,
+  false,
+  new Date(1995, 10, 11),
+  {
+    titulo: "Si pudieras verme ahora",
+    autor: "Cecelia Ahern",
+    fecha: new Date("2005, 11, 01"),
+    url: "https://www.amazon.com/-/es/Cecelia-Ahern/dp/8496778223",
+  },
+];
