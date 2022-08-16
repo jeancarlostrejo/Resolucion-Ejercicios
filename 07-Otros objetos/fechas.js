dba426ff8a1a436d27534bac6778e9a0c917c849
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2022-08-16 16:33:43 -0400

    Add ejercicio 07 - otros objetos III

---
+++ 07-Otros objetos/fechas.js
@@ -0,0 +1,33 @@
+/**
+ * Crea un archivo llamado fechas.js que contenga las siguientes líneas
+
+- La fecha de hoy
+
+- La fecha de tu nacimiento
+
+- Un variable que indique si hoy es más tarde (>) que la fecha de tu nacimiento
+
+- Una variable que contenga el día de tu nacimiento
+
+- Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
+
+- Una variable que contenga el año de tu nacimiento (con 4 dígitos)
+ */
+
+const hoy = new Date();
+console.log(hoy);
+
+const fechaNacimiento = new Date(1995, 10, 11);
+console.log(fechaNacimiento);
+
+const comparar = hoy > fechaNacimiento;
+console.log(comparar);
+
+const dia = fechaNacimiento.getDate();
+console.log(dia);
+
+const mes = fechaNacimiento.getMonth() + 1;
+console.log(mes);
+
+const anho = fechaNacimiento.getFullYear();
+console.log(anho);
